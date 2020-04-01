67badd743090a27ca48802a9dd11f839a29dce3a
vikram chand <vikram.wri231@webreinvent.com>
2020-04-01 15:23:48 +0530

    fetch list and view detail

---
+++ src/Resources/views/backend/vaahone/vue/app/pages/permission/View.vue
@@ -0,0 +1,91 @@
+<script src="./ViewJs.js"></script>
+<template>
+    <div class="column" v-if="page.assets">
+        <div class="block" v-if="is_content_loading">
+            <Loader/>
+        </div>
+
+        <div class="card" v-else>
+            <!--header-->
+            <header class="card-header">
+
+                <div v-if="item" class="card-header-title">
+                    <span>#{{item[0].value}} / </span>
+                    <span>{{item[1].value}}</span>
+                </div>
+
+                <div class="card-header-buttons">
+
+                    <div class="field has-addons is-pulled-right">
+                        <p class="control">
+                            <b-button icon-left="edit"
+                                      :loading="is_btn_loading"
+                                      @click="create('save')">
+                                Save
+                            </b-button>
+                        </p>
+
+                        <p class="control">
+
+
+                            <b-dropdown aria-role="list" position="is-bottom-left">
+                                <button class="button" slot="trigger">
+                                    <b-icon icon="caret-down"></b-icon>
+                                </button>
+
+                                <b-dropdown-item aria-role="listitem">
+                                    <b-icon icon="check"></b-icon>
+                                    Save & Close
+                                </b-dropdown-item>
+                                <b-dropdown-item aria-role="listitem">
+                                    <b-icon icon="plus"></b-icon>
+                                    Save & New
+                                </b-dropdown-item>
+
+                            </b-dropdown>
+
+
+                        </p>
+
+                        <p class="control">
+                            <b-button tag="router-link"
+                                      :to="{name: 'perm.list'}"
+                                      icon-left="times">
+                            </b-button>
+                        </p>
+
+
+
+                    </div>
+
+
+                </div>
+
+            </header>
+            <!--/header-->
+
+            <!--content-->
+            <div class="card-content">
+
+                <div class="block" >
+
+
+                    content
+
+                </div>
+            </div>
+            <!--/content-->
+
+
+
+
+
+        </div>
+
+
+
+
+    </div>
+</template>
+
+
---
+++ src/Resources/views/backend/vaahone/vue/app/pages/permission/partials/ListLargeView.vue
@@ -0,0 +1,77 @@
+<script src="./ListLargeViewJs.js"></script>
+<template>
+    <div>
+        <h4 class="is-4">large view</h4>
+        <b-table :data="page.list_is_empty ? [] : page.list.data"
+                 :checkable="true"
+                 :checked-rows.sync="page.bulk_action.selected_items"
+                 checkbox-position="left"
+                 :hoverable="true"
+                 :row-class="setRowClass"
+        >
+
+            <template slot-scope="props">
+                <b-table-column field="id" label="ID" width="40" numeric>
+                    {{ props.row.id }}
+                </b-table-column>
+
+                <b-table-column field="name" label="Name">
+                    {{ props.row.name }}
+                </b-table-column>
+
+                <b-table-column field="email" label="Slug">
+                    {{ props.row.slug }}
+                </b-table-column>
+
+                <b-table-column field="status" label="Is Active">
+                    <b-button v-if="props.row.is_active === 1" rounded size="is-small" type="is-primary" @click="changeStatus">Yes</b-button>
+                    <b-button v-else rounded size="is-small" type="is-danger" @click="changeStatus">No</b-button>
+<!--                    <span v-if="props.row.is_active === 1" class="tag is-primary">-->
+<!--                        Yes-->
+<!--                    </span>-->
+<!--                    <span v-else class="tag is-danger">-->
+<!--                        No-->
+<!--                    </span>-->
+                </b-table-column>
+
+                <b-table-column field="created_at" label="Roles" >
+                    {{ props.row.count_roles }}
+                </b-table-column>
+
+                <b-table-column field="gender" label="Users">
+                    {{ props.row.count_users }}
+                </b-table-column>
+
+                <b-table-column field="created_at" label="Created At">
+                    {{ $vaah.fromNow(props.row.created_at) }}
+                </b-table-column>
+
+
+                <b-table-column field="actions" label=""
+                                width="40">
+
+                    <b-tooltip label="View" type="is-dark">
+                        <b-button size="is-small"
+                                  @click="setActiveItem(props.row)"
+                                  icon-left="chevron-right">
+                        </b-button>
+                    </b-tooltip>
+
+
+                </b-table-column>
+
+
+            </template>
+
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>Nothing here.</p>
+                    </div>
+                </section>
+            </template>
+
+        </b-table>
+    </div>
+</template>
+
