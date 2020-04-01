4906026b1658b086f976bfe65961b892d1ffce06
vikram chand <vikram.wri231@webreinvent.com>
2020-04-01 15:50:01 +0530

    update top menu

--- src/Resources/views/backend/default/pages/permissions.blade.php
+++ src/Resources/views/backend/default/pages/permissions.blade.php
@@ -6,7 +6,7 @@
 
 
 @section('vaahcms_extend_admin_js')
-    <script src="{{vh_get_admin_assets("builds/app-permission.js")}}" defer></script>
+    <script src="{{vh_get_admin_assets("builds/app-permissions.js")}}" defer></script>
 @endsection
 
 @section('content')
